991337eadcb50f8f2dd540f12f044e6fc1880834
jacks0nwill <70932263+jacks0nwill@users.noreply.github.com>
2020-09-19 13:13:21 -0600

    Added GreenPipe

---
+++ src/levelPieces/GreenPipe.java
@@ -0,0 +1,22 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class GreenPipe extends GamePiece {
+	//if player lands on the green pipe it advances the level
+	public GreenPipe(char symbol, String name, int location) {
+		super(symbol, name, location);
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		if(playerLocation==super.getLocation()) {
+			return(InteractionResult.ADVANCE);
+		}
+		else {
+			return(InteractionResult.NONE);
+		}
+	}
+
+}
